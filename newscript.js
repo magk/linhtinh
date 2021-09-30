4be97f23f72d0bce8f2e6bc1541a99106ed4a1c5
magk <bluevnd@gmail.com>
2021-09-30 14:02:17 +0700

    add new script
    to xuongintem

---
+++ newscript.js
@@ -0,0 +1,153 @@
+    {
+        "@context": "http://schema.org",
+        "@type": "LocalBusiness",
+        "@id": "https://xuongintem.com/",
+        "url": "https://xuongintem.com/",
+        "logo": "https://xuongintem.com/wp-content/uploads/2021/01/cropped-logo-xuongintem.jpg",
+        "image": "",
+        "priceRange": "0 VND- 5.000.000 VND",
+        "hasMap": "https://www.google.com/maps?cid=3902399318828557497", 
+        "email": "xuongintemnhan@gmail.com",
+        "founder": "Bảo Tín",
+        "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Quận Gò Vấp",
+            "addressCountry": "VIỆT NAM",
+            "addressRegion": "Hồ Chí Minh",
+            "postalCode": "700000",
+            "streetAddress": "27/18 Thống Nhất, Phường 10, Gò Vấp, Thành phố Hồ Chí Minh 70000"
+        },
+        "description": "1000 + Mẫu In Tem Nhãn ✓ Tem Sticker ✓ In Hộp Giấy ✓ In Catalogue ✓ Sản Phẩm Cao Cấp ✓ Giá Tốt Nhất ✓Phí Giao Hàng Thấp ✓ Click Vào Đây!
+",
+        "name": "Bảo Tín",
+        "telephone": "0984 880 378",
+        "openingHoursSpecification": [{
+                "@type": "OpeningHoursSpecification",
+                "dayOfWeek": [
+                    "Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday",	  
+					"Saturday"
+                ],
+                "opens": "08:00",
+                "closes": "17:30"
+            },
+            {
+                "@type": "OpeningHoursSpecification",
+                "dayOfWeek": [
+                    "Saturday"
+                ],
+                "opens": "08:00",
+                "closes": "17:30"
+            }
+        ],
+        "geo": {
+            "@type": "GeoCoordinates",
+            "latitude": "10.8423177",
+            "longitude": "106.662657,17z"
+        },
+        "potentialAction": {
+            "@type": "ReserveAction",
+            "target": {
+                "@type": "EntryPoint",
+                "urlTemplate": "https://xuongintem.com/",
+                "inLanguage": "vn",
+                "actionPlatform": [
+                    "http://schema.org/DesktopWebPlatform",
+                    "http://schema.org/IOSPlatform",
+                    "http://schema.org/AndroidPlatform"
+                ]
+            },
+            "result": {
+                "@type": "Reservation",
+                "name": "Đăng Ký"
+            }
+        },
+
+        "sameAs": ["https://twitter.com/xuongintembt",
+            " https://www.pinterest.com/inbaotingiatot",
+            " https://flipboard.com/@xuongintembt",
+            " https://myspace.com/xuongintembtin",
+            " https://www.instapaper.com/p/9496153",
+            " https://tapas.io/inbaotingiatot",
+            " https://www.folkd.com/user/xuongintembtin?"
+        ]
+    }
+</script>
+<script type="application/ld+json">
+    {
+        "@context": "http://schema.org",
+        "@type": "Person",
+        "name": "Bảo Tín",
+        "jobTitle": "Founder",
+        "image": " https://xuongintem.com/wp-content/uploads/2021/01/cropped-logo-xuongintem.jpg",
+        "worksFor": "xuongintem.com",
+        "url": " https://xuongintem.com",
+        "sameAs": ["https://www.tumblr.com/blog/xuongintembt",
+            " https://trello.com/giatotbaotin ",
+            " https://www.flickr.com/people/193930114@N03/",
+            " https://band.us/band/85311878",
+            " https://www.hahalolo.com/@61401c21a9a84e5a723883a3 "
+        ],
+        "AlumniOf": ["Bảo Tín",
+            "Bảo Tín"
+        ],
+        "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Hồ Chí Minh",
+            "addressRegion": "vietnam"
+        }
+    }
+</script>
+
+    <title> Xưởng In Bảo Tín Chuyên In Tem Nhãn ✓ Tem Sticker Cao Cấp
+ </title>
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org/",
+      "@type": "Product",
+      "name": " Xưởng In Bảo Tín Chuyên In Tem Nhãn ✓ Tem Sticker Cao Cấp",
+      "image":
+        " https://xuongintem.com/wp-content/uploads/2021/01/cropped-logo-xuongintem.jpg",
+      "description": " 1000 + Mẫu In Tem Nhãn ✓ Tem Sticker ✓ In Hộp Giấy ✓ In Catalogue ✓ Sản Phẩm Cao Cấp ✓ Giá Tốt Nhất ✓Phí Giao Hàng Thấp ✓ Click Vào Đây!
+
+",
+      "sku": "0446310786",
+      "mpn": "925872",
+      "brand": {
+        "@type": "Brand",
+        "name": "In Bảo Tín"
+      },
+      "review": {
+        "@type": "Review",
+        "reviewRating": {
+          "@type": "Rating",
+          "ratingValue": "4",
+          "bestRating": "5"
+        },
+        "author": {
+          "@type": "Person",
+          "name": "Bảo Tín"
+        }
+      },
+      "aggregateRating": {
+        "@type": "AggregateRating",
+        "ratingValue": "4.4",
+        "reviewCount": "89"
+      },
+      "offers": {
+        "@type": "Offer",
+        "url": " https://xuongintem.com/",
+        "priceCurrency": "VND",
+        "price": "45000",
+        "priceValidUntil": "2021-09-14",
+        "itemCondition": "https://schema.org/UsedCondition",
+        "availability": "https://schema.org/InStock",
+        "seller": {
+          "@type": "Organization",
+          "name": "Bảo Tín"
+        }
+      }
+    }
